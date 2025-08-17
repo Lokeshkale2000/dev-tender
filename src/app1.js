a3ef172681c3eb8fad9993d836d3bc02e04e4b41
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2025-08-17 23:35:11 +0530

    dynamic data in database

--- src/app1.js
+++ src/app1.js
@@ -2,20 +2,22 @@ const express = require("express");
 
 const app = express();
 const { connectionDB } = require("./database");
-const {UserModel}= require('./schema')
+const { UserModel } = require("./schema");
 
 const port = 8000;
 
-app.post("/insertuserdata", async(req, res) => {
-    const user = new UserModel({
-        firstName: "lokesh",
-        lastName: "Kale",
-        email:"lokeshkale22002@gmail.com"
-    })
-    await user.save();
-    res.send("user added successfully")
-    console.log(user)
+app.use(express.json());
 
+app.post("/insertuserdata", async (req, res) => {
+  // creating a new instance of the user model
+  const user = new UserModel(req.body);
+  try {
+    await user.save();
+    res.send("user added successfully");
+    console.log(user);
+  } catch (err) {
+    res.status(400).send("error saving the user" + err.message);
+  }
 });
 connectionDB()
   .then(() => {
