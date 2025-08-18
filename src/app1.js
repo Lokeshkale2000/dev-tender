df04ac790ea1df2946b79f69adaf4c3b2c60e4cc
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2025-08-18 13:42:39 +0530

    edit/update

--- src/app1.js
+++ src/app1.js
@@ -24,15 +24,14 @@ app.post("/insertuserdata", async (req, res) => {
 //get all the data from the users
 app.get("/getuserdata", async (req, res) => {
   try {
-    const user = await UserModel.find({})
+    const user = await UserModel.find({});
     res.send(user);
   } catch (err) {
-    res.status(400).send("something went wrong")
-    
+    res.status(400).send("something went wrong");
   }
 });
 
-// find one data from the user by mail 
+// find one data from the user by mail
 app.get("/user", async (req, res) => {
   const usermail = req.body.email;
   try {
@@ -40,14 +39,34 @@ app.get("/user", async (req, res) => {
     if (user.length === 0) {
       res.status(404).send("user not found");
     } else {
-       res.send(user);
+      res.send(user);
     }
-
-   
   } catch (err) {
     res.status(400).send("something went wrong");
   }
 });
+
+//delete the user
+app.delete("/deleteuser", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await UserModel.findByIdAndDelete({_id:userId});
+    console.log(user)
+    res.send("user delete successfully");
+  } catch (err) {}
+});
+
+//edit the userdata
+app.patch("/updatedata", async (req, res) => {
+  const data = req.body;
+  const userId= req.body.userId
+  try {
+    User = await UserModel.findByIdAndUpdate({ _id: userId }, data)
+    res.send("user updated successfully");
+  } catch (err) {
+    console.log("something went wrong")
+  }
+})
 connectionDB()
   .then(() => {
     app.listen(port, () => {
